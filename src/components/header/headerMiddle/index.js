6fbbfc6e1875f2d580a27035c5e9c220ed3ac335
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-01-28 18:00:02 +0300

    Make home link work

--- src/components/header/headerMiddle/index.js
+++ src/components/header/headerMiddle/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './headerMiddle.css';
 
 export default function HeaderMiddle() {
   return (
     <div className="inlineHeader middleHeader">
-      <span className="item">HOME</span>
+      <Link to="/">
+        <span className="item">HOME</span>
+      </Link>
       <span className="item">MEDICINE</span>
       <span className="item">UPDATES</span>
     </div>
